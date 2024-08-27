ac616cff02d24b809bbb5b1173070cb5b4ab1241
James C <james.craig@bellrocktechnology.com>
2024-08-27 23:57:33 +0100

    Update to gitignore file, cypress config file and initial test file.

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern: 'cypress/e2e/exploratory/*.js'
   },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/login.cy.ts",
+  },
+
+  projectId: "4m261g",
 });
--- cypress/e2e/exploratory/initial_tests.js
+++ cypress/e2e/exploratory/initial_tests.js
@@ -1,12 +1,39 @@
-describe('Initial Test', function (){
-    it('Not do much ', function() {
+/// <reference types="cypress" />
+
+describe('Cypress Studio Demo', () => {});
+
+context('login', () => {
+    beforeEach(() => {
+        //it('.type() - type into a DOM element', () => {
         cy.visit('https://test.bellrock.dev')
-        cy.viewport(1803, 662)
-        cy.get('#login-email').click()
-        cy.get('#login-email').type('james.craig+1@bellrocktechnology.com')
+        cy.get('#login-email')
+            .type('james.craig+1@bellrocktechnology.com').should('have.value', 'james.craig+1@bellrocktechnology.com')
         cy.get('#next-button').click()
-        cy.get('#password').type('T3stUs3r!')
-        cy.get('#password').click()
+        cy.get('#password').type('T3stUs3r!',{ parseSpecialCharSequences: false })
         cy.get('#login-button').click()
-    });
+        cy.wait(5000)
+        cy.get('header').should('exist')
+        cy.location().then((location) => {
+            //cy.wrap(location.href).should(
+            //"contain",
+            //"#nav-dashboard"
+            //)
+            cy.end()
+        })
+        //})
+    })
+
+    it('Can open Lightkeeper', () => {
+        cy.get('.admin-app > img:nth-child(1)').click()
+        //   .should('have.class', 'focus')
+        //   .prev().should('have.attr', 'style', 'color: orange;')
+        cy.wait(20000)
+        cy.location().then((location) => {
+            cy.wrap(location.href).should(
+                "contain",
+                "/lumen-lightkeeper/agents/view"
+            )
+            cy.end()
+        })
+    })
 })
